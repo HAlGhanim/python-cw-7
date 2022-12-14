b5ac6e1659095c7e4b6a1f80e3212f5cd39161a2
HAlGhanim <imzaji98@gmail.com>
2022-12-14 19:37:05 +0300

    cw7

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
+# الجزء الاول
+class Person:
+    
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    def __str__(self):
+        return f"my name is {self.name} and I am {self.age} years old"
+    # def is_adult(self):
+    #     if self.age > 18:
+    #         print("You have too much responsibilities")
+    #     elif self.age < 18:
+    #         print("Lucky you")
 
-# write your code here
+
+# first_person = Person()
+# print(first_person.name)
+# print(first_person.age)
+
+# الجزء الثاني
+# first_person.is_adult()
+
+# الجزء الثالث
+first_person = Person("Humoud", 23)
+print(first_person)
